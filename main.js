d37818dc9d5fc4c1b855b86bd2487399e7b90b3d
Alexander <50184502+alexander970@users.noreply.github.com>
2022-03-12 19:05:44 +0100

    fetching API and Authorization

--- main.js
+++ main.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log("hello world")
+console.log("hello world");
+
+//Autorizacion de API buscador de vuelos de Amadeus
+fetch("https://test.api.amadeus.com/v1/security/oauth2/token", {
+  method: "POST",
+  body: `grant_type=client_credentials&client_id=${encodeURIComponent("2GWP7ADgQGQ4iyFZWi1XwaINtedn96PK")}&client_secret=${encodeURIComponent("qkCP4skIgwt5s7AD")}`,
+  headers: {
+    "Content-type": "application/x-www-form-urlencoded",
+  },
+})
+.then(function (response) {
+    if (response.ok) {
+        return response.json();
+    }
+    throw response;
+})
+.then(function (data) {
+    console.log(data);
+    console.log(data["access_token"]);
+})
+.catch(function (error) {
+    console.error(error);
+});
+ 
