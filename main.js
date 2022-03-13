44b7df1075dc0a663d03e247cb1529b73db24c11
Alexander <50184502+alexander970@users.noreply.github.com>
2022-03-13 21:02:38 +0100

    example of use

--- main.js
+++ main.js
@@ -4,27 +4,50 @@ console.log("hello world");
 //Autorizacion de API buscador de vuelos de Amadeus
 fetch("https://test.api.amadeus.com/v1/security/oauth2/token", {
   method: "POST",
-  body: `grant_type=client_credentials&client_id=${encodeURIComponent("2GWP7ADgQGQ4iyFZWi1XwaINtedn96PK")}&client_secret=${encodeURIComponent("qkCP4skIgwt5s7AD")}`,
+  body: `grant_type=client_credentials&client_id=${encodeURIComponent(
+    "2GWP7ADgQGQ4iyFZWi1XwaINtedn96PK"
+  )}&client_secret=${encodeURIComponent("qkCP4skIgwt5s7AD")}`,
   headers: {
     "Content-type": "application/x-www-form-urlencoded",
   },
 })
-.then(function (response) {
+  .then(function (response) {
     if (response.ok) {
-        return response.json();
+      return response.json();
     }
     throw response;
-})
-.then(function (data) {
+  })
+  .then(function (data) {
     console.log(data);
     console.log(data["access_token"]);
-})
-.catch(function (error) {
+  })
+  .catch(function (error) {
     console.error(error);
-});
- 
+  });
 
 
-fetch('./airports.json')
-.then(res => res.json())
-.then(data => console.log(data["MAD"]));
+
+  //Validation function
+function validateIataCode(airports, code) {
+  if (code.length !== 3) {
+    return false;
+  }
+  if (code in airports) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+fetch("./airports.json")
+  .then((res) => res.json())
+  .then((airports) => {
+    console.log(validateIataCode(airports, "MAD"));
+    console.log(airports["MAD"]);
+    const code = "MAD";
+    const isCodeIataValid = validateIataCode(airports,"MAD");
+    if (isCodeIataValid ===true){
+      console.log(airports[code]);
+    }
+    
+  });
