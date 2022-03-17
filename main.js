bedcba33ab523c43053b99564cdd4656908fc378
Alexander <50184502+alexander970@users.noreply.github.com>
2022-03-18 00:41:11 +0100

    Code organization and improvements

--- main.js
+++ main.js
@@ -1,164 +1,282 @@
 "use strict";
 
+function convertDateToApiFormat(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const monthPadded = month.toString().padStart(2, "0");
+  const day = date.getDate();
+  const apiDate = `${year}-${monthPadded}-${day}`;
+
+  return apiDate;
+}
+
+async function getFlightOffers(
+  accessToken,
+  originAirportIata,
+  destinationAirportIata,
+  departureDate
+) {
+  try {
+    const departureDateFormatted = convertDateToApiFormat(departureDate);
+
+    const response = await fetch(
+      `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${originAirportIata}&destinationLocationCode=${destinationAirportIata}&departureDate=${departureDateFormatted}&adults=1&nonStop=false&max=250`,
+
+      {
+        method: "GET",
+        headers: {
+          accept: "application/vnd.amadeus+json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw "Ha ocurrido un error al intentar obtener las ofertas de los vuelos.";
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw "Ha ocurrido un error al intentar obtener las ofertas de los vuelos.";
+  }
+}
+
+async function getAmadeusApiToken() {
+  try {
+    const response = await fetch(
+      "https://test.api.amadeus.com/v1/security/oauth2/token",
+      {
+        method: "POST",
+        body: `grant_type=client_credentials&client_id=${encodeURIComponent(
+          "2GWP7ADgQGQ4iyFZWi1XwaINtedn96PK"
+        )}&client_secret=${encodeURIComponent("qkCP4skIgwt5s7AD")}`,
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded",
+        },
+      }
+    );
+
+    if (!response.ok) {
+      throw "Ha ocurrido un error al intentar obtener el token.";
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw "Ha ocurrido un error al intentar obtener el token.";
+  }
+}
+
 Date.prototype.nextDay = function () {
   const date = new Date(this.valueOf());
   date.setDate(date.getDate() + 1);
   return date;
 };
 
-function convertDateToApiFormat(date){
-  const year = date.getFullYear();
-  const month = date.getMonth()+1;
-  const monthPadded= month.toString().padStart(2,"0");
-  const day = date.getDate();
- const apiDate= `${year}-${monthPadded}-${day}`;
- return apiDate;
+function getNextDayDate() {
+  const currentDate = new Date();
+  const nextDayDate = currentDate.nextDay();
+  return nextDayDate;
 }
 
-function loadFlightOffers(originAirportIata, destinationAirportIata) {
+async function getFlightOffersWithToken(
+  originAirportIata,
+  destinationAirportIata
+) {
   //Autorizacion de API buscador de vuelos de Amadeus
-  fetch("https://test.api.amadeus.com/v1/security/oauth2/token", {
-    method: "POST",
-    body: `grant_type=client_credentials&client_id=${encodeURIComponent(
-      "2GWP7ADgQGQ4iyFZWi1XwaINtedn96PK"
-    )}&client_secret=${encodeURIComponent("qkCP4skIgwt5s7AD")}`,
-    headers: {
-      "Content-type": "application/x-www-form-urlencoded",
-    },
-  })
-    .then(function (response) {
-      if (response.ok) {
-        return response.json();
-      }
-      throw response;
-    })
-    .then(function (data) {
-      console.log(data);
-      const accessToken = data["access_token"];
+  const tokenData = await getAmadeusApiToken();
+  const accessToken = tokenData["access_token"];
 
-      const currentDate = new Date();
-      const myApiDate = convertDateToApiFormat(currentDate.nextDay());
+  //Obtener la fecha de mañana
+  const departureDate = getNextDayDate();
 
+  //Obtener ofertas de vuelo desde la API
+  const flightOffers = await getFlightOffers(
+    accessToken,
+    originAirportIata,
+    destinationAirportIata,
+    departureDate
+  );
 
-
-      fetch(
-        `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${originAirportIata}&destinationLocationCode=${destinationAirportIata}&departureDate=${myApiDate}&adults=1&nonStop=false&max=250`,
-        {
-          method: "GET",
-          headers: {
-            accept: "application/vnd.amadeus+json",
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      )
-        .then(function (response2) {
-          if (response2.ok) {
-            return response2.json();
-          }
-          throw response2;
-        })
-        .then(function (flightOffers) {
-          displayCheapestFlight(flightOffers.data);
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
-    });
+  return flightOffers.data;
 }
 
-//Validation function
-function validateIataCode(airports, code) {
+function displayError(message) {
+  const offer = document.querySelector(".offer");
+  offer.innerText = message;
+}
+
+async function getAirports() {
   try {
-    if (code.length !== 3) {
-      throw new Error("El código debe tener tres letras");
+    const response = await fetch("./airports.json");
+
+    if (!response.ok) {
+      throw "Ha ocurrido un error al intentar obtener los aeropuertos.";
     }
-    if (code in airports) {
-      return true;
-    }
-    if (code !== NaN) {
-      throw new Error("El código solo puede tener letras");
-    } else {
-      throw new Error(
-        "El código introducido no corresponde a ningún aeropueto"
-      );
-    }
-  } catch (e) {
-    console.error("Se ha producido un error " + e.message);
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw "Ha ocurrido un error al intentar obtener los aeropuertos.";
   }
 }
 
-/* fetch("./airports.json")
-  .then((res) => res.json())
-  .then((airports) => {
-    console.log(validateIataCode(airports, "SCQ"));
-    console.log(airports["SCQ"]);
-    const code = "SCQ";
-    const isCodeIataValid = validateIataCode(airports, "SCQ");
+async function getAirlines() {
+  try {
+    const response = await fetch("./airlines.json");
 
-    if (isCodeIataValid === true) {
-      console.log(airports[code]);
+    if (!response.ok) {
+      throw "Ha ocurrido un error al intentar obtener las aerolíneas.";
     }
-  }); */
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw "Ha ocurrido un error al intentar obtener las aerolíneas.";
+  }
+}
+
+function displayCheapestFlight(
+  originAirportName,
+  destinationAirportName,
+  airlineName,
+  price,
+  departureTime
+) {
+  const offer = document.querySelector(".offer");
+  const timeElement = document.querySelector(".time");
+  const airlineElement = document.querySelector(".airline");
+  const priceElement = document.querySelector(".price");
+
+  offer.innerText = `El vuelo más económico para mañana entre ${originAirportName} y ${destinationAirportName} es:`;
+  airlineElement.innerText = `Aerolinea: ${airlineName}`;
+  priceElement.innerText = `Precio: ${price} €`;
+  timeElement.innerText = `Horario: ${departureTime}`;
+}
+
+function clearCheapestFlight() {
+  const offer = document.querySelector(".offer");
+  const timeElement = document.querySelector(".time");
+  const airlineElement = document.querySelector(".airline");
+  const priceElement = document.querySelector(".price");
+
+  offer.innerText = "";
+  airlineElement.innerText = "";
+  priceElement.innerText = "";
+  timeElement.innerText = "";
+}
+
+function getAndDisplayCheapestFlight(
+  airlines,
+  originAirport,
+  destinationAirport
+) {
+  getFlightOffersWithToken(originAirport.IATA, destinationAirport.IATA)
+    .then((flightOffers) => {
+      if (flightOffers.length > 0) {
+        const flightOffersSorted = [...flightOffers];
+        flightOffersSorted.sort(
+          (offerA, offerB) => offerA.price.grandTotal - offerB.price.grandTotal
+        );
+        const cheapestFlightOffer = flightOffersSorted[0];
+
+        const airlineCode =
+          cheapestFlightOffer.validatingAirlineCodes[0].toUpperCase();
+        const airlineName =
+          airlineCode in airlines ? airlines[airlineCode] : airlineCode;
+        const price = cheapestFlightOffer.price.grandTotal;
+        const departureTime =
+          cheapestFlightOffer.itineraries[0].segments[0].departure.at;
+
+        displayCheapestFlight(
+          originAirport.Name,
+          destinationAirport.Name,
+          airlineName,
+          price,
+          departureTime
+        );
+      } else {
+        offer.innerText = `No hay vuelos para mañana entre ${originAirport.Name} y ${destinationAirport.Name}`;
+      }
+    })
+    .catch(displayError);
+}
+
+//Función de validación
+function validateIataCodes(
+  airports,
+  originAirportIata,
+  destinationAirportIata
+) {
+  const isOriginAirportIataValid = originAirportIata in airports;
+  const isDestinationAirportIataValid = destinationAirportIata in airports;
+
+  return { isOriginAirportIataValid, isDestinationAirportIataValid };
+}
+
+function displayCheapestFlightIfValid(
+  airports,
+  airlines,
+  originAirportIata,
+  destinationAirportIata
+) {
+  const { isOriginAirportIataValid, isDestinationAirportIataValid } =
+    validateIataCodes(airports, originAirportIata, destinationAirportIata);
+
+  if (isOriginAirportIataValid && isDestinationAirportIataValid) {
+    const originAirport = {
+      IATA: originAirportIata,
+      Name: airports[originAirportIata],
+    };
+
+    const destinationAirport = {
+      IATA: destinationAirportIata,
+      Name: airports[destinationAirportIata],
+    };
+
+    getAndDisplayCheapestFlight(airlines, originAirport, destinationAirport);
+  } else {
+    if (isOriginAirportIataValid) {
+      displayError(
+        `El código de aeropuerto "${destinationAirportIata}" no es válido.`
+      );
+    } else {
+      displayError(`El código aeropuerto "${originAirportIata}" no es válido.`);
+    }
+  }
+}
 
 //Forms
-function printAeropuertos(e) {
+function handleFlightsFormSubmit(e) {
   e.preventDefault();
-  console.log(origenInput.value);
-  console.log(destinoInput.value);
 
   const originAirportIata = origenInput.value.toUpperCase();
   const destinationAirportIata = destinoInput.value.toUpperCase();
-  const originAirport = document.querySelector(".origin");
-  const destinationAirport = document.querySelector(".destination");
 
- fetch("./airports.json")
-    .then((res) => res.json())
+  clearCheapestFlight();
+
+  getAirports()
     .then((airports) => {
-      console.log(validateIataCode(airports, "MAD"));
-      console.log(airports["MAD"]);
-      const code = "MAD";
-      const isCodeIataValid = validateIataCode(airports, "MAD"); 
-
-      if (isCodeIataValid === true) {
-        console.log(airports[code]);
-      }
-      const offer = document.querySelector(".offer")
-      offer.innerText = `El vuelo más económico entre ${airports[originAirportIata]} y ${airports[destinationAirportIata]} mañana es:`
-      /* originAirport.innerText = `Aeropuerto de origen: ${airports[originAirportIata]}`;
-      destinationAirport.innerText = `Aeropuerto de destino: ${airports[destinationAirportIata]}`; */
-      loadFlightOffers(originAirportIata, destinationAirportIata);
+      getAirlines().then((airlines) => {
+        displayCheapestFlightIfValid(
+          airports,
+          airlines,
+          originAirportIata,
+          destinationAirportIata
+        );
+      });
+    })
+    .catch((error) => {
+      displayError(error);
     });
-    
-    this.reset();
-  }
-  
-  const flightsForm = document.querySelector(".flightsForm");
-  const { origen: origenInput, destino: destinoInput } = flightsForm.elements;
-  
-  flightsForm.addEventListener("submit", printAeropuertos);
-  
-  function displayCheapestFlight(flightOffers) {
-    const flightOffersSorted = [...flightOffers];
-    flightOffersSorted.sort(
-      (offerA, offerB) => offerA.price.grandTotal - offerB.price.grandTotal
-      );
-      const cheapestFlightOffer = flightOffersSorted[0];
-      console.log(cheapestFlightOffer);
-      
-      
-  const time = document.querySelector(".time")
-  const airline = document.querySelector(".airline");
-  const price = document.querySelector(".price");
-  airline.innerText = `Aerolinea: ${cheapestFlightOffer.validatingAirlineCodes[0]}`;
-  price.innerText = `Precio: ${cheapestFlightOffer.price.grandTotal} €`;
-  time.innerText = `Horario: ${cheapestFlightOffer.itineraries[0].segments[0].departure.at}`
-  
 }
 
+const flightsForm = document.querySelector(".flightsForm");
+const { origen: origenInput, destino: destinoInput } = flightsForm.elements;
 
-
-
-
-
-
-
+flightsForm.addEventListener("submit", handleFlightsFormSubmit);
